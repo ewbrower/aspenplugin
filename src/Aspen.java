0567a3b52544dc22b34ccc339073e3fbf05ee073
ewbrower <ewbrower@gmail.com>
2017-03-14 07:59:25 -0400

    init of Aspen file - main plugin driver

---
+++ src/Aspen.java
@@ -0,0 +1,22 @@
+import com.nomagic.magicdraw.plugins.Plugin;
+
+/**
+ * Created by ewbrower on 3/14/17.
+ */
+
+
+
+public class Aspen extends Plugin {
+
+    public void init() {
+        
+    }
+
+    public boolean close() {
+        return false;
+    }
+
+    public boolean isSupported() {
+        return false;
+    }
+}
